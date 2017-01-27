67bb5273105fd1a1e1f305b554e495315adfda5e
Administrator <Administrator@STUDENT09>
2017-01-27 14:47:14 +0000

    Updated Author and Book

--- module04/src/main/java/Trai/Author.java
+++ module04/src/main/java/Trai/Author.java
@@ -1,8 +1,26 @@
 package Trai;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Author {
+	
+	@Id
+	@GeneratedValue
+	private int author_id;
 
+	@Column(name = "title")
+	private String full_name;
+
+	@OneToMany(cascade = CascadeType.ALL)
+	private Set<Book> Books = new HashSet<>();
 }
+
--- module04/src/main/java/Trai/Book.java
+++ module04/src/main/java/Trai/Book.java
@@ -1,8 +1,26 @@
 package Trai;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
 
 @Entity
 public class Book {
+	@Id
+	@GeneratedValue
+	private int bookID;
+	
+	@Column(name = "title")
+	private String title;
 
+	@ManyToOne(cascade = CascadeType.ALL)
+	private Set<Author> author = new HashSet<>();
+	
 }
